0e223d64beb25b256280244714e6fbe8c2f8e3cc
jiyoung <cwd05011@gmail.com>
2023-02-02 22:25:36 +0900

    rename: 셀렉트 상수 값 위치 변경

--- src/component/Searcher/index.tsx
+++ src/component/Searcher/index.tsx
@@ -1,15 +1,10 @@
 import { useState } from "react";
 import { ISearcherProps, TPROPERTIES_SEARCH } from "./types";
 import SearcherView from "./View";
+import { PROPERTIES_SEARCH } from "../../constants/index";
 
 const Searcher = () => {
   const [selectValue, SetSelectValue] = useState("oldAddress");
-
-  const PROPERTIES_SEARCH: TPROPERTIES_SEARCH[] = [
-    { property: "artName", propertyTitle: "작품 이름으로 검색" },
-    { property: "oldAddress", propertyTitle: "옛 주소로 검색" },
-    { property: "roadAddress", propertyTitle: "도로명 주소로 검색" }
-  ];
 
   const SearcherProps: ISearcherProps = {
     PROPERTIES_SEARCH,
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,11 @@
+import { TPROPERTIES_SEARCH } from "../component/Searcher/types";
+
 export const WELCOME_RENDER_TIME = 2000;
 
-export const MENU_TITLE = ['Agora', 'Suggest', 'Reference'];
+export const MENU_TITLE = ["Agora", "Suggest", "Reference"];
+
+export const PROPERTIES_SEARCH: TPROPERTIES_SEARCH[] = [
+  { property: "artName", propertyTitle: "작품 이름으로 검색" },
+  { property: "oldAddress", propertyTitle: "옛 주소로 검색" },
+  { property: "roadAddress", propertyTitle: "도로명 주소로 검색" }
+];
