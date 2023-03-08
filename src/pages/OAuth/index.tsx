e4c7260353fb4a0ce0e5963e9c6efb66e41f5ad1
jiyoung <cwd05011@gmail.com>
2023-03-08 16:02:37 +0900

    test: 로그인 쿠키 테스팅

--- src/pages/OAuth/index.tsx
+++ src/pages/OAuth/index.tsx
@@ -1,12 +1,15 @@
 import Cookies from "js-cookie";
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export const OAuth = () => {
-  const location = useLocation();
-  const accessToken = Cookies.get("access_token");
-  const webId = Cookies.get("webid");
-  console.log("accessToken", accessToken);
-  console.log("location", location.pathname);
-  console.log("webId", webId);
+  useEffect(() => {
+    const location = useLocation();
+    const accessToken = Cookies.get("access_token");
+    const webId = Cookies.get("webid");
+    console.log("accessToken", accessToken);
+    console.log("location", location.pathname);
+    console.log("webId", webId);
+  }, []);
   return <div> auth page</div>;
 };
